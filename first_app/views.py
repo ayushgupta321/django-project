f8a4f3756183326cae132c67bd2ffcedf2f5d07e
ayush <master.ayushg@gmail.com>
2019-03-06 09:27:23 +0530

    First Commit

---
+++ first_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    text_dict = {"text_here": "This is good"}
+    return render(request,"first_app/index.html",context=text_dict)
+
+
+    
