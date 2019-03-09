6c955bf5fb671d862fd970c71fae3099aa23f6c7
ayush <master.ayushg@gmail.com>
2019-03-09 13:06:44 +0530

    models

--- first_project/urls.py
+++ first_project/urls.py
@@ -19,5 +19,6 @@ from django.urls import include,path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('first_app.urls'))
+    path('',include('first_app.urls')),
+    path('user/',include('user.urls'))
 ]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from user.models import User
 
 # Create your views here.
+def index(request):
+    data = {"data":User.objects.order_by("first_name")}
+    return render(request,"user/user_page.html",context=data)
